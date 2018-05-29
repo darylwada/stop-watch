b29ecd743d7e80d8d231386e8cc9da6ec786baae
Daryl Wada <daryl.wada@gmail.com>
2018-05-29 12:56:44 -0700

    Added global variable to check if watch is currently running. #2

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var $startBtn = document.querySelector('.start-btn')
 var $elapsedTime = document.querySelector('.elapsed-time')
+var timerRunning = false
 
 function updateTime() {
   $elapsedTime.textContent = parseInt($elapsedTime.textContent) + 1
 }
 
 function startTimer() {
+  timerRunning = !timerRunning
   setInterval(updateTime, 1000)
 }
 

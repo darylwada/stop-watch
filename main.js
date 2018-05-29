1127e6466d50fde31a3e32d54e22f09d288faea7
Daryl Wada <daryl.wada@gmail.com>
2018-05-29 13:03:50 -0700

    Added a stopTimer function to clear the interval when the start buton is clicked while it is running. #2

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var $startBtn = document.querySelector('.start-btn')
 var $elapsedTime = document.querySelector('.elapsed-time')
 var timerRunning = false
+var timerInterval
+
+function stopTimer() {
+  clearInterval(timerInterval)
+}
 
 function updateTime() {
   $elapsedTime.textContent = parseInt($elapsedTime.textContent) + 1
@@ -8,7 +13,12 @@ function updateTime() {
 
 function startTimer() {
   timerRunning = !timerRunning
-  setInterval(updateTime, 1000)
+  if (timerRunning) {
+    stopTimer()
+  }
+  else {
+    timerInterval = setInterval(updateTime, 1000)    
+  }
 }
 
 $startBtn.addEventListener('click', startTimer)

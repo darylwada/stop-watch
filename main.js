799f6dc903879d24770d13842420883145067eef
Daryl Wada <daryl.wada@gmail.com>
2018-05-29 11:24:57 -0700

    Defined a function that updates the elapsed seconds and writes it to the DOM. #1

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var $startBtn = document.querySelector('.start-btn')
+var $elapsedTime = document.querySelector('.elapsed-time')
+
+function updateTime() {
+  $elapsedTime.textContent = parseInt($elapsedTime.textContent) + 1
+}
 
 function startTimer() {
 

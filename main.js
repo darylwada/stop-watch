ae96a2d5152201de96b1e510f6669d9eb66d4047
Daryl Wada <daryl.wada@gmail.com>
2018-05-30 13:39:41 -0700

    User can start the stop watch. Original code commented out for testing purposes. #9

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-var stopWatchState = {
-  isStarted: false,
-  timeElapsed: 0,
-  timeLimit: 5
-}
 
 function renderTimeLimit(stopWatchState) {
   var $timeLimitContainer = document.createElement('div')
@@ -52,6 +47,7 @@ function renderStartBtn(stopWatchState) {
 }
 
 function renderStopWatch(stopWatchState) {
+  document.body.innerHTML = ''
   var $container = document.createElement('div')
   var $header = document.createElement('h1')
   $header.textContent = 'Stop Watch'
@@ -64,56 +60,79 @@ function renderStopWatch(stopWatchState) {
   return $container
 }
 
-document.body.appendChild(renderStopWatch(stopWatchState))
+function startInterval() {
+  timerInterval = setInterval(function() {
+    stopWatchState.isStarted = true
+    stopWatchState.timeElapsed += 1
+    document.body.prepend(renderStopWatch(stopWatchState))
+  }, 1000)
+}
 
-
-var $startBtn = document.querySelector('.start-btn')
-var $resetBtn = document.querySelector('.reset-btn')
-var $elapsedTime = document.querySelector('.elapsed-time')
-var $timeLimit = document.querySelector('#time-limit-input')
-var timerRunning = false
+var stopWatchState = {
+  isStarted: false,
+  timeElapsed: 0,
+  timeLimit: 5
+}
 var timerInterval
 
-function stopTimer() {
-  clearInterval(timerInterval)
-}
+document.body.prepend(renderStopWatch(stopWatchState))
 
-function updateTime() {
-  var elapsedTime = parseInt($elapsedTime.textContent)
-  var timeLimit = parseInt($timeLimit.value)
-  if (elapsedTime === timeLimit) {
-    stopTimer()
-    $elapsedTime.classList.add('expired')
-  }
-  else {
-    $elapsedTime.textContent = elapsedTime + 1
-  }
-}
+var $startBtn = document.querySelector('.start-btn')
+$startBtn.addEventListener('click', startInterval)
 
-function startTimer() {
-  if (timerRunning) {
-    stopTimer()
-    $startBtn.textContent = 'Start'
-    $startBtn.classList.remove('started')
-  }
-  else {
-    timerInterval = setInterval(updateTime, 1000)
-    $startBtn.textContent = 'Pause'
-    $startBtn.classList.add('started')
-    $resetBtn.classList.remove('hidden')
-  }
-  timerRunning = !timerRunning
-}
 
-function resetTimer() {
-  stopTimer()
-  $elapsedTime.textContent = 0
-  $elapsedTime.classList.remove('expired')
-  $startBtn.textContent = 'Start'
-  $startBtn.classList.remove('started')
-  $resetBtn.classList.add('hidden')
-  timerRunning = false
-}
 
-$startBtn.addEventListener('click', startTimer)
-$resetBtn.addEventListener('click', resetTimer)
+
+
+
+
+// var $startBtn = document.querySelector('.start-btn')
+// var $resetBtn = document.querySelector('.reset-btn')
+// var $elapsedTime = document.querySelector('.elapsed-time')
+// var $timeLimit = document.querySelector('#time-limit-input')
+// var timerRunning = false
+// var timerInterval
+//
+// function stopTimer() {
+//   clearInterval(timerInterval)
+// }
+//
+// function updateTime() {
+//   var elapsedTime = parseInt($elapsedTime.textContent)
+//   var timeLimit = parseInt($timeLimit.value)
+//   if (elapsedTime === timeLimit) {
+//     stopTimer()
+//     $elapsedTime.classList.add('expired')
+//   }
+//   else {
+//     $elapsedTime.textContent = elapsedTime + 1
+//   }
+// }
+//
+// function startTimer() {
+//   if (timerRunning) {
+//     stopTimer()
+//     $startBtn.textContent = 'Start'
+//     $startBtn.classList.remove('started')
+//   }
+//   else {
+//     timerInterval = setInterval(updateTime, 1000)
+//     $startBtn.textContent = 'Pause'
+//     $startBtn.classList.add('started')
+//     $resetBtn.classList.remove('hidden')
+//   }
+//   timerRunning = !timerRunning
+// }
+//
+// function resetTimer() {
+//   stopTimer()
+//   $elapsedTime.textContent = 0
+//   $elapsedTime.classList.remove('expired')
+//   $startBtn.textContent = 'Start'
+//   $startBtn.classList.remove('started')
+//   $resetBtn.classList.add('hidden')
+//   timerRunning = false
+// }
+//
+// $startBtn.addEventListener('click', startTimer)
+// $resetBtn.addEventListener('click', resetTimer)
